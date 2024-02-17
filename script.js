1d4195d127a123ff63440c5cfb1c8fb0f8e9e938
Adebisi Abdussamad <adebisiabdussamadayomide@gmail.com>
2024-02-17 23:07:02 +0100

    Created scenario for the check button

--- script.js
+++ script.js
@@ -10,13 +10,31 @@ console.log(document.querySelector('.guess').value);
 */
 
 
-// Handing Clicks of a Button
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
+// Handing Clicks of a Button
 document.querySelector('.check').addEventListener('click', function () {
 	const guess = Number(document.querySelector('.guess').value);
 	console.log(guess, typeof guess);
 
+	// No number
 	if (!guess) {
 		document.querySelector('.message').textContent = '⛔ No number!';
+	}
+
+	// When the guess is equal to the secret number!
+	else if (guess === secretNumber) {
+		document.querySelector('.message').textContent = '🎉 Correct Number!';
+	}
+
+	// When the guess is greater than the secret number
+	else if (guess > secretNumber) {
+		document.querySelector('.message').textContent = '📈 Too high!'
+	}
+
+	// When the guess is lower than the secret number
+	else if (guess < secretNumber) {
+		document.querySelector('.message').textContent = '📉 Too low!'
 	}
 })
