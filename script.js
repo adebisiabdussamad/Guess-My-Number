e50377f5f19bf263c33d4a429e16a7a0a5dff234
Adebisi Abdussamad <adebisiabdussamadayomide@gmail.com>
2024-02-17 22:50:07 +0100

    Implementing the game logic

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+/*
+document.querySelector('.message').textContent = 'Hello, world!';
+
+document.querySelector('.number').textContent = 15;
+document.querySelector('.score').textContent = 14;
+
+document.querySelector('.guess').value = 32
+console.log(document.querySelector('.guess').value);
+*/
+
+
+// Handing Clicks of a Button
+
+document.querySelector('.check').addEventListener('click', function () {
+	const guess = Number(document.querySelector('.guess').value);
+	console.log(guess, typeof guess);
+
+	if (!guess) {
+		document.querySelector('.message').textContent = '⛔ No number!';
+	}
+})
